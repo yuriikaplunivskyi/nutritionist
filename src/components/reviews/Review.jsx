471857defd6b17a9ec29f23a8ad80a4779bd403a
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-10-18 23:31:47 +0300

    feat: animation effects for review component

--- src/components/reviews/Review.jsx
+++ src/components/reviews/Review.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import "./review.scss";
-import { motion, useScroll, useSpring} from "framer-motion";
+import { motion, useScroll, useSpring, useTransform} from "framer-motion";
 
 const items = [
     {
@@ -34,9 +34,31 @@ const items = [
 ]
 
 const Single = ({item}) => {
+
+    const ref = useRef(); 
+
+    const {scrollYProgress} = useScroll(
+        {
+        target: ref,
+        //offset:["end end", "start start"]
+    });
+
+    const y = useTransform(scrollYProgress, [0,1 ], [-300, 300])
+
     return (
-        <section>
-            {item.title}
+        <section >
+            <div className="container">
+                <div className="wrapper">
+                    <div className="imageContainer" ref={ref}>
+                        <img src={item.img} alt="" />
+                    </div>
+                    <motion.div className="textContainer" style={{y}}>
+                        <h2>{item.title}</h2>
+                        <p>{item.desc}</p>
+                        <button>Глянути</button>
+                    </motion.div>             
+                </div>
+            </div>
         </section>
     )
 }
@@ -45,12 +67,17 @@ const Review = () => {
 
     const ref = useRef()
 
-    const {scrollYProgress} = useScroll({target: ref, offset:["end end", "start start"]})
+    const {scrollYProgress} = useScroll(
+        {
+        target: ref,
+        offset:["end end", "start start"]
+    });
 
     const scaleX = useSpring( scrollYProgress, {
         stiffness: 100,
         damping: 30
-    })
+    });
+
     return (
         <div className="review" ref={ref}> 
             <div className="progress">
