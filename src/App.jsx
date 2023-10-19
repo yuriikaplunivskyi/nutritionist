57a22fe3753368fa8b1946bdce5e036c4d433c62
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-10-19 21:19:24 +0300

    feat: animat mouse cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./app.scss"
 import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/navbar";
 import Parallax from "./components/parallax/Parallax";
@@ -8,6 +9,7 @@ import Services from "./components/services/Services";
 
 const App = () => {
   return <div>
+    <Cursor/>
     <section id="homepage">
       <Navbar/>
       <Hero/>
