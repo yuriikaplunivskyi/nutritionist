9623bbab3aa2a1c22ef592171bab687bc432c615
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-11-15 23:14:03 +0200

    feat: about

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,58 +7,8 @@ import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Review from "./components/reviews/Review";
 import Services from "./components/services/Services";
-import Slider from "./components/slider/Slider";
 
-const slides = [
-  {
-    id: 1, 
-    img: "/certificates/noDiet-nutritionists.jpg",
-    school: "Nodiet school",
-    title: "Нутріціологія"
-  },
-  {
-    id: 2, 
-    img: "/certificates/iFit-group-fit.png",
-    school: "iFIT",
-    title: "Інструктор групових занять"
-  },
-  {
-    id: 3, 
-    img: "/certificates/fa-bodyMobility.png",
-    school: "Fitness academy",
-    title: "Body Mobility"
-  },
-  {
-    id: 4, 
-    img: "/certificates/fa-dprpm.png",
-    school: "Fitness academy",
-    title: "Діагностика, профілактика та вирішення проблеми м'язового дисбалансу"
-  },
-  {
-    id: 5, 
-    img: "/certificates/fa-func_training.png",
-    school: "Fitness academy",
-    title: "Функціональний тренінг/НІІТ"
-  },
-  {
-    id: 6, 
-    img: "/certificates/fa-pumpIt.png",
-    school: "Fitness academy",
-    title: "Силове тренування IT UP & TABS"
-  },
-  {
-    id: 7, 
-    img: "/certificates/fa-master-trainer_2l.png",
-    school: "Fitness academy",
-    title: "Майстер тренажерної зали (рівень ІІ)"
-  },
-  {
-    id: 8, 
-    img: "/certificates/fa-master-trainer_3l.png",
-    school: "Fitness academy",
-    title: "Майстер тренажерної зали (рівень ІІІ)"
-  }
-];
+
 
 const App = () => {
   return <div>
@@ -68,15 +18,12 @@ const App = () => {
       <Hero/>
     </section>
     <section>
-      <About/>
-    </section>
-    <section>
       <Parallax type="reviews"/>
     </section>
-    <Review/>
-    <section id="certs">
-      <Slider slides={slides}/>
+    <section>
+      <About/>
     </section>
+    <Review/>
     <section id="about">
       <Parallax type="services"/>
     </section>
--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,9 +1,69 @@
-import "./about.scss"
+import "./about.scss";
+import Slider from "../slider/Slider";
+
+const slides = [
+    {
+      id: 1, 
+      img: "/certificates/noDiet-nutritionists.jpg",
+      school: "Nodiet school",
+      title: "Нутриціологія"
+    },
+    {
+      id: 2, 
+      img: "/certificates/iFit-group-fit.png",
+      school: "iFIT",
+      title: "Інструктор групових занять"
+    },
+    {
+      id: 3, 
+      img: "/certificates/fa-bodyMobility.png",
+      school: "Fitness academy",
+      title: "Body Mobility"
+    },
+    {
+      id: 4, 
+      img: "/certificates/fa-dprpm.png",
+      school: "Fitness academy",
+      title: "Діагностика, профілактика та вирішення проблеми м'язового дисбалансу"
+    },
+    {
+      id: 5, 
+      img: "/certificates/fa-func_training.png",
+      school: "Fitness academy",
+      title: "Функціональний тренінг/НІІТ"
+    },
+    {
+      id: 6, 
+      img: "/certificates/fa-pumpIt.png",
+      school: "Fitness academy",
+      title: "Силове тренування PUMP IT UP & TABS"
+    },
+    {
+      id: 7, 
+      img: "/certificates/fa-master-trainer_2l.png",
+      school: "Fitness academy",
+      title: "Майстер тренер тренажерної зали (рівень ІІ)"
+    },
+    {
+      id: 8, 
+      img: "/certificates/fa-master-trainer_3l.png",
+      school: "Fitness academy",
+      title: "Майстер тренер тренажерної зали (рівень ІІІ)"
+    }
+    ];
 
 const About = () => {
     return (
-        <div className="about">About</div>
+        <div className="about">
+            <div className="about-imgContainer">
+                <img src="/whoami.jpg" alt="eee" />
+            </div>
+            <Slider slides={slides}/>
+            <h3 className="about-title">Я сертифікований фітнес-тренер та нутриціолог, health-coach</h3>
+            <p className="about-text">Вже більше 2-х років я прививаю любов до спорту та збалансованого харчуванню своїм клієнтам. допомагаю людям робити здоровий вибір у харчуванні та житті, набувати фігури мрії, повертати енергію та значно покращувати здоров&apos;я. Я можу вам показати великі можливості збалансованого харчування, а не обмеження, привити любов до фізичної активності без болей і втоми</p>
+            
+        </div>
     )
 }
 
-export default About
+export default About;
