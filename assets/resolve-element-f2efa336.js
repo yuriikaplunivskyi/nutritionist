eb139a49d764896bc7b66c347b03724bbf17c1ad
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2023-12-01 00:55:00 +0200

    Updates

--- assets/resolve-element-f2efa336.js
+++
@@ -1,1 +0,0 @@
-import{l as a}from"./motion-c9686223.js";function v(r,o,i){var f;if(typeof r=="string"){let u=document;o&&(a(!!o.current),u=o.current),i?((f=i[r])!==null&&f!==void 0||(i[r]=u.querySelectorAll(r)),r=i[r]):r=u.querySelectorAll(r)}else r instanceof Element&&(r=[r]);return Array.from(r||[])}export{v as r};
