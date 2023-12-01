6fe20ce19666eac5c6b78622d9ac9f2f180f7a7d
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2023-12-01 11:32:07 +0200

    Updates

--- assets/resolve-element-fc0645f0.js
+++
@@ -1,1 +0,0 @@
-import{l as a}from"./motion-8ca11c22.js";function v(r,o,i){var f;if(typeof r=="string"){let u=document;o&&(a(!!o.current),u=o.current),i?((f=i[r])!==null&&f!==void 0||(i[r]=u.querySelectorAll(r)),r=i[r]):r=u.querySelectorAll(r)}else r instanceof Element&&(r=[r]);return Array.from(r||[])}export{v as r};
