923d5ee62b572a7e6ba296c474fb635cc0c2ad91
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2023-12-08 23:31:18 +0200

    Updates

---
+++ assets/resolve-element-b9b9158e.js
@@ -0,0 +1,1 @@
+import{s as a}from"./index-88e19f79.js";function v(r,o,i){var f;if(typeof r=="string"){let u=document;o&&(a(!!o.current),u=o.current),i?((f=i[r])!==null&&f!==void 0||(i[r]=u.querySelectorAll(r)),r=i[r]):r=u.querySelectorAll(r)}else r instanceof Element&&(r=[r]);return Array.from(r||[])}export{v as r};
