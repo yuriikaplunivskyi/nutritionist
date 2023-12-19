4f02cfb5d80515ef71009f01beb799365a6c2176
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2023-12-19 19:46:51 +0200

    refactore: styles

--- client/src/components/formService/FormService.jsx
+++ client/src/components/formService/FormService.jsx
@@ -6,13 +6,11 @@ import { useForm } from 'react-hook-form';
 const variants = {
     initial: {
         x: 500,
-        y: 50,
         opacity: 0,
         
     },
     animate: {
         x: 0,
-        y: 0,
         opacity: 1,
         transition: {
             duration: 0.5
