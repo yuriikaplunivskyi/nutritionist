de428209370fedd35cb1fe01b5d7ad806f8c66fa
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-01-02 23:32:36 +0200

    feat: empty components + new rout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import "./app.scss";
 
 import Home from "./pages/Home";
 import ServicePage from "./pages/ServicePage";
+import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
     <Routes >
       <Route path="/" element={<Home />}/>
       <Route path="/service/:serviceId" element={<ServicePage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,25 @@
+import Loader from "../components/loader/Loader";
+import Cursor from "../components/cursor/Cursor";
+import "./notfound.scss";
+import  {lazy, Suspense } from "react";
+import { Link } from "react-router-dom";
+import AddServices from "../components/addServices/AddServices";
+export const LazyNavbar = lazy(() => import("../components/navbar/Navbar"));
+/* export const LazyCursor = lazy(() => import("../components/cursor/Cursor")); */
+
+const DashboardPage = () => {
+    return (
+        <>
+            <Cursor/>
+            <Suspense fallback={<div className="loader-container"><Loader/></div>}>
+            <LazyNavbar/>
+            <section className="notfound">
+                <AddServices/>
+                <Link className="notfound-btn" to="/">Повернутися на головну</Link>
+            </section>
+            </Suspense>
+        </>
+    )
+}
+
+export default DashboardPage;
