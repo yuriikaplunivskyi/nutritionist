87e98264d5d48fd7eeb72f14cbdf836f6d99112b
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-01-11 23:20:57 +0200

    fix: bug replacing data between tables

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,37 @@ app.get("/services", (req, res) => {
     })
 })
 
+// Додати цей маршрут перед рештою маршрутів
+app.get("/services/:id", (req, res) => {
+    const serviceId = req.params.id;
+    const q = `
+        SELECT
+            id,
+            title,
+            descr,
+            path,
+            icon,
+            passing,
+            prices,
+            warnings
+        FROM
+            service
+        WHERE
+            id = ?;
+    `;
+
+    db.query(q, [serviceId], (err, data) => {
+        if (err) return res.json(err);
+        
+        if (data.length === 0) {
+            return res.status(404).json({ error: "Service not found" });
+        }
+
+        res.json(data[0]); 
+    });
+});
+
+
 app.post("/services", (req, res) => {
     const q = 
     "INSERT INTO service (`title`, `descr`, `passing`, `prices`, `warnings`, `path`, `icon`) VALUE (?)";
@@ -89,6 +120,28 @@ app.delete("/services/:id", (req, res) => {
         return res.json("Service has been deleted Successfully");
     });
 })
+
+app.put("/services/:id", (req, res) => {
+    const serviceId = req.params.id;
+    const q = "UPDATE service SET `title`=?, `descr`=?, `path`=?, `icon`=?, `passing`=?, `prices`=?, `warnings`=? WHERE id = ?";
+
+    const values = [
+        req.body.title,
+        req.body.descr,
+        req.body.path,
+        req.body.icon,
+        JSON.stringify(req.body.passing),
+        JSON.stringify(req.body.prices),
+        JSON.stringify(req.body.warnings),
+        serviceId,
+    ];
+
+    db.query(q, values, (err, data) => {
+        if (err) return res.json(err);
+        return res.json("Service has been updated Successfully");
+    });
+});
+
 
 app.listen(8800, () => {
     console.log("Connected to backend!")
--- client/src/components/update/Update.jsx
+++ client/src/components/update/Update.jsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import "./update.scss";
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const Update = () => {
@@ -8,12 +11,15 @@ const Update = () => {
         title:"",
         descr:"",
         path:"",
-        icon:"",
         passing: [],
         prices: [],
         warnings: [],
+        icon:"",
     };
 
+    const location = useLocation();
+
+    const serviceId = location.pathname.split("/")[2];
     const [service, setService] = useState(initStateService);
 
     const handleChange = (e) => {
@@ -45,78 +51,98 @@ const Update = () => {
         updatedArray.splice(index, 1);
         setService({ ...service, [arrayName]: updatedArray });
     };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8800/services/${serviceId}`);
+                console.log(response.data);
+                const parsedData = {
+                    ...response.data,
+                    passing: JSON.parse(response.data.passing),
+                    prices: JSON.parse(response.data.prices),
+                    warnings: JSON.parse(response.data.warnings),
+                };
     
+                setService(parsedData);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData();
+    }, [serviceId]);
 
     const handleClick = async e => {
         e.preventDefault();
         try {
-            await axios.post("http://localhost:8800/services", service);
+            await axios.put("http://localhost:8800/services/"+ serviceId, service);
             setService(initStateService);
         } catch (error) {
             console.log(error);
         }
     }
     return (
-        <div className='formAddServices'>
+        <div className='updateForm'>
             <div className="container">
-                <h6>Додай нову послугу</h6>
-                <input type="text" name="title" value={service.title} onChange={handleChange} placeholder="Назва послуги"/>
-                <textarea cols="30" rows="10" value={service.descr} name="descr" onChange={handleChange} placeholder="Опис послуги"/>
-                <input type="text" name="path" value={service.path} onChange={handleChange} placeholder="Шлях до послуги"/>
-                <div>
-                    <h6>Passing</h6>
-                    {service.passing.map((item, index) => (
-                        <div key={index}>
-                            <input
-                                type="text"
-                                value={item.stage}
-                                onChange={(e) => handleArrayChange(index, "stage", e.target.value, 'passing')}
-                                placeholder={`Passing ${index + 1}`}
-                            />
-                            <button className="formAddServices-delete btn" onClick={() => handleRemoveArray(index, 'passing')}>Видалити</button>
-                        </div>
-                    ))}
-                    <button className="formAddServices-add btn" onClick={() => handleAddArray('passing', { stage: "", id: service.passing.length + 1 })}>Додати Passing</button>
-                </div>
-                <div>
-                    <h6>Prices</h6>
-                    {service.prices.map((item, index) => (
-                        <div key={index}>
-                            <input
-                                type="text"
-                                value={item.price}
-                                onChange={(e) => handleArrayChange(index, "price", e.target.value, 'prices')}
-                                placeholder={`Prices ${index + 1}`}
-                            />
-                            <button className="formAddServices-delete btn" onClick={() => handleRemoveArray(index, 'prices')}>Видалити</button>
-                        </div>
-                    ))}
-                    <button className="formAddServices-add btn" onClick={() => handleAddArray('prices', { price: "", id: service.prices.length + 1 })}>Додати Prices</button>
-                </div>
-                <div>
-                    <h6>Warnings</h6>
-                    {service.warnings.map((item, index) => (
-                        <div key={index}>
-                            <input
-                                type="text"
-                                value={item.title}
-                                onChange={(e) => handleArrayChange(index, "title", e.target.value, 'warnings')}
-                                placeholder={`Title ${index + 1}`}
-                            />
-                            <input
-                                type="text"
-                                value={item.content}
-                                onChange={(e) => handleArrayChange(index, "content", e.target.value, 'warnings')}
-                                placeholder={`Content ${index + 1}`}
-                            />
-                            <button className="formAddServices-delete btn" onClick={() => handleRemoveArray(index, 'warnings')}>Видалити</button>
-                        </div>
-                    ))}
-                    <button className="formAddServices-add btn" onClick={() => handleAddArray('warnings', { title: "", content: "", id: service.warnings.length + 1 })}>Додати Warnings</button>
-                </div>
+                    <h6>Змінити послугу</h6>
+                    <input type="text" name="title" value={service.title} onChange={handleChange} placeholder="Назва послуги"/>
+                    <textarea cols="30" rows="10" value={service.descr} name="descr" onChange={handleChange} placeholder="Опис послуги"/>
+                    <input type="text" name="path" value={service.path} onChange={handleChange} placeholder="Шлях до послуги"/>
+                    <div>
+                        <h6>Passing</h6>
+                        {service.passing.map((item, index) => (
+                            <div key={item.id}>
+                                <input
+                                    type="text"
+                                    value={item.stage}
+                                    onChange={(e) => handleArrayChange(index, "stage", e.target.value, 'passing')}
+                                    placeholder={`Passing ${index + 1}`}
+                                />
+                                <button className="updateForm-delete btn" onClick={() => handleRemoveArray(index, 'passing')}>Видалити</button>
+                            </div>
+                        ))}
+                        <button className="updateForm-add btn" onClick={() => handleAddArray('passing', { stage: "", id: service.passing.length + 1 })}>Додати Passing</button>
+                    </div>
+                    <div>
+                        <h6>Prices</h6>
+                        {service.prices.map((item, index) => (
+                            <div key={index}>
+                                <input
+                                    type="text"
+                                    value={item.price}
+                                    onChange={(e) => handleArrayChange(index, "price", e.target.value, 'prices')}
+                                    placeholder={`Prices ${index + 1}`}
+                                />
+                                <button className="updateForm-delete btn" onClick={() => handleRemoveArray(index, 'prices')}>Видалити</button>
+                            </div>
+                        ))}
+                        <button className="updateForm-add btn" onClick={() => handleAddArray('prices', { price: "", id: service.prices.length + 1 })}>Додати Prices</button>
+                    </div>
+                    <div>
+                        <h6>Warnings</h6>
+                        {service.warnings.map((item, index) => (
+                            <div key={index}>
+                                <input
+                                    type="text"
+                                    value={item.title}
+                                    onChange={(e) => handleArrayChange(index, "title", e.target.value, 'warnings')}
+                                    placeholder={`Title ${index + 1}`}
+                                />
+                                <input
+                                    type="text"
+                                    value={item.content}
+                                    onChange={(e) => handleArrayChange(index, "content", e.target.value, 'warnings')}
+                                    placeholder={`Content ${index + 1}`}
+                                />
+                                <button className="updateForm-delete btn" onClick={() => handleRemoveArray(index, 'warnings')}>Видалити</button>
+                            </div>
+                        ))}
+                        <button className="updateForm-add btn" onClick={() => handleAddArray('warnings', { title: "", content: "", id: service.warnings.length + 1 })}>Додати Warnings</button>
+                    </div>
 
-                <button className="formAddServices-send btn" onClick={handleClick}>Відправити на сервер</button>
-            </div>
+                    <button className="updateForm-send btn" onClick={handleClick}>Внести зміни</button>
+                </div>
+                <Link to="/dashboard">Повернутися в адмінку</Link>
         </div>
     )
 }
