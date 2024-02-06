da49ca1daea49eabf0ab0a644123fda7e366ecb6
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-02-06 20:07:00 +0200

    куафсещку

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,14 @@ const __dirname = dirname(__filename);
 const app = express();
 
 app.use(cors());
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Access-Control-Allow-Credentials", "true");
 
     next();
-});
+}); */
 
 
 const db = mysql.createConnection({
