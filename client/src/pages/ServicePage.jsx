7fdcf58c5518fae04a78b8f09309c52238d7cb86
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-02-08 23:25:00 +0200

    --еуіе

--- client/src/pages/ServicePage.jsx
+++ client/src/pages/ServicePage.jsx
@@ -1,24 +1,29 @@
 import Loader from "../components/loader/Loader";
-
 import Cursor from "../components/cursor/Cursor";
-import  {lazy, Suspense } from "react";
-export const LazyService = lazy(() => import("../components/service/Service"));
-export const LazyNavbar = lazy(() => import("../components/navbar/Navbar"));
-export const LazyFormService = lazy(() => import("../components/contact/Contact"));
+import { lazy, Suspense } from "react";
+import useServiceData from '../../hooks/useServiceData';
 
+const LazyService = lazy(() => import("../components/service/Service"));
+const LazyNavbar = lazy(() => import("../components/navbar/Navbar"));
+const LazyFormService = lazy(() => import("../components/contact/Contact"));
 
 const ServicePage = () => {
+    const { serviceData } = useServiceData();
+
+    if (!serviceData) {
+        return <Loader />;
+    }
 
     return (
-        <main >
-            <Cursor/>
-            <Suspense fallback={<div className="loader-container"><Loader/></div>}>
+        <main>
+            <Cursor />
+            <Suspense fallback={<div className="loader-container"><Loader /></div>}>
                 <LazyNavbar />
                 <LazyService />
-                <LazyFormService/>
+                <LazyFormService />
             </Suspense>
         </main>
-    )
-}
+    );
+};
 
-export default ServicePage
+export default ServicePage;
