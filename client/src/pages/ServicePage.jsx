5b39f2ec69a2ab9749664a5c3e83a4dbcc0bd3e2
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-02-13 16:17:11 +0200

    --refactore

--- client/src/pages/ServicePage.jsx
+++ client/src/pages/ServicePage.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useEffect } from "react";
+import { lazy, Suspense } from "react";
 import Loader from "../components/loader/Loader";
 import Cursor from "../components/cursor/Cursor";
 import useServiceData from '../hooks/useServiceData';
