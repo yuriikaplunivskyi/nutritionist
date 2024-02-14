ce83f9a801173a98e9578832b3f7bd7fcf4bf446
yuriikaplunivskyi <92335421+yuriikaplunivskyi@users.noreply.github.com>
2024-02-14 16:41:47 +0200

    refactore: styles about comp and text in about and rewiews

--- client/dist/assets/dots-loading-586fb599.js
+++
@@ -1,1 +0,0 @@
-const o=""+new URL("../icon/dots-loading.gif",import.meta.url).href;export{o as d};
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,25 @@
+
+import "./app.scss";
 import  {lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from './components/loader/Loader.jsx';
 import ScrollToAnchor from './hooks/ScrollToAnchor.js';
-import "./app.scss";
+import RequireAuth from "./components/authorization/RequireAuth";
 export const Home = lazy(() => import("./pages/Home"));
 export const ServicePage = lazy(() => import("./pages/ServicePage"));
-export const Dashboard = lazy(() => import("./pages/dashboard/Dashboard"));
-export const NotFound = lazy(() => import("./pages/NotFound"));
-export const UpdateService = lazy(() => import("./components/dashboard/updateService/UpdateService"));
+/* export const Dashboard = lazy(() => import("./pages/dashboard/Dashboard"));
+export const UpdateService = lazy(() => import("./components/dashboard/updateService/UpdateService")); 
 export const PasswordReset = lazy(() => import("./components/authorization/passwordReset/PasswordReset"));
-export const Admin = lazy(() => import("./pages/admin/Admin"));
-export const Signup = lazy(() => import("./components/authorization/signup/Signup"));
+export const Admin = lazy(() => import(""));
+export const Signup = lazy(() => import("./components/authorization/signup/Signup"));*/
 
-import RequireAuth from "./components/authorization/RequireAuth";
+import NotFound from "./pages/NotFound";
+import Dashboard from "./pages/dashboard/Dashboard";
+import UpdateService from "./components/dashboard/updateService/UpdateService";
+import PasswordReset from "./components/authorization/passwordReset/PasswordReset";
+import Admin from "./pages/admin/Admin";
+import Signup from "./components/authorization/signup/Signup";
+
 
 
 const App = () => {
@@ -32,18 +39,18 @@ const App = () => {
               <ServicePage />
             </Suspense> }
           />
-          <Route path="/admin" element={<Suspense fallback={<div className="loader-container"><Loader /></div>}><Admin /></Suspense>} />
-          <Route path="/admin/reset" element={<Suspense fallback={<div className="loader-container"><Loader /></div>}><PasswordReset /></Suspense>} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/reset" element={<PasswordReset />} />
           <Route 
             path="/admin/signup" 
-            element={<Suspense fallback={<div className="loader-container"><Loader /></div>}><RequireAuth><Signup /></RequireAuth></Suspense>} 
+            element={<RequireAuth><Signup /></RequireAuth>} 
           />
           <Route 
             path="/admin/dashboard" 
-            element={ <Suspense fallback={<div className="loader-container"><Loader /></div>}><RequireAuth><Dashboard /></RequireAuth></Suspense>}/>
+            element={<RequireAuth><Dashboard /></RequireAuth>}/>
           <Route 
             path="/admin/update/:id" 
-            element={<Suspense fallback={<div className="loader-container"><Loader /></div>}><RequireAuth><UpdateService /></RequireAuth></Suspense>} 
+            element={<RequireAuth><UpdateService /></RequireAuth>} 
           />
       </Routes>
     
